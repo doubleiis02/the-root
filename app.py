e95f669e4c6fe53819ea4912090134647cc7b4d1
vekslerav <vekslerav@gmail.com>
2020-11-22 11:19:34 -0800

    App.py connected with index.html

--- app.py
+++ app.py
@@ -1,49 +1,17 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-Bootstrap(app)
-
-##FORMS##
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4,max=15)])
-    password = PasswordField('password', validators = [InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4,max=15)])
-    password = PasswordField('password', validators = [InputRequired(), Length(min=8, max=80)]) 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
 
-    if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
-
-    return render_template('login.html', form=form)
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
-
-    return render_template('signup.html', form=form)
-
-@app.route('/homepage')
-def homepage():
-    return render_template('homepage.html')
+@app.route('/about')
+def about():
+    print("This is the about page")
+    return "Welcome to the about page"
 
 if __name__== '__main__':
     app.run(debug=True)
+
