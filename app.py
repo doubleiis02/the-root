1e5cadc7580b0d5ec1727335c591c1cc8562aed1
vekslerav <vekslerav@gmail.com>
2020-11-29 20:21:21 -0800

    Adds create a class and choose color functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import pymongo
 
 app = Flask(__name__)
 
@@ -15,7 +16,6 @@ class_list = [Classes('English 1B', '#64dfd4', '#83D4CD'), Classes('English 1C',
 
 # class_list = [Classes('Science', '#000000', '#cc6464')]
 
-
 @app.route('/')
 def index():
     return render_template('index.html', current_classes=current_classes, class_list=class_list)
@@ -29,13 +29,21 @@ def about():
 if __name__== '__main__':
     app.run(debug=True)
 
-classes = ["class 1", "class 2"]
 @app.route('/add_class', methods = ['POST', 'GET'])
 def add_class():
     if request.method == 'POST':
         name = request.form['class-name-input']
+        primary_color = request.form['pri-class-color-input']
+        secondary_color = request.form['sec-class-color-input']
     else:
         name = request.args.get('class-name-input')
-    classes.append(name)
-    print(classes)
-    return render_template('index.html')
+        primary_color = request.args.get('pri-class-color-input')
+        secondary_color = request.args.get('sec-class-color-input')
+    new = Classes(name, primary_color, secondary_color)
+    class_list.append(new)
+    print(class_list)
+    return render_template('index.html', class_list=class_list)
+
+@app.route('/add_class_page')
+def add_class_page():
+    return render_template('addClass.html')
